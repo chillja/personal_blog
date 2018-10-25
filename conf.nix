9a3d4fae1e55384058104616d581d460242a6d0b
Ilja Becker <mail@iljabecker.eu>
2018-10-25 07:19:03 +0200

    conf: fix siteUrl

--- conf.nix
+++ conf.nix
@@ -3,7 +3,7 @@
   /* URL of the site, must be set to the url of the domain the site will be deployed.
      Should not end with a '/'.
   */
-  siteUrl = "http://chillja.github.io";
+  siteUrl = "http://chillja.github.io/personal_blog";
 
   /* Theme specific settings
      it is possible to override any of the used themes configuration in this set.
